57c3d5edb78f854379f99481edf5f53ff69de18f
inhibitor1217 <cytops1217@gmail.com>
2018-01-06 16:31:27 +0900

    Asset
    asset

---
+++ Assets/Scripts/LazerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerController : MonoBehaviour {
+
+    public float Speed;
+
+    private const float MAX_LENGTH = 2.0f;
+
+    private Vector2 _head, _tail;    
+    private float _theta;
+    
+    private bool _isCreating = true;
+
+    private float _constantY;
+    private float _constantXScale, _constantZScale;
+
+    private Vector2 unitVector(float angle) {
+        float radAngle = Mathf.Deg2Rad * angle;
+        return new Vector2(Mathf.Cos(radAngle), Mathf.Sin(radAngle));
+    }
+
+    private void Start() {
+
+        Vector3 eulerRotation = transform.rotation.eulerAngles;
+        _theta = -eulerRotation.y;
+
+        float initialScale = transform.localScale.y;
+
+        Vector2 position2D = new Vector2(transform.localPosition.x, transform.localPosition.z);
+        
+        _head = position2D + initialScale * Vector2.right;
+        _tail = position2D - initialScale * Vector2.right;
+
+        _constantY      = transform.localPosition.y;
+        _constantXScale = transform.localScale.x;
+        _constantZScale = transform.localScale.z;
+
+    }
+
+    private void Update() {
+
+        _head += Vector2.left * Speed * Time.deltaTime;
+
+        if (!_isCreating) {
+            _tail += Vector2.left * Speed * Time.deltaTime;
+        }
+
+        // finish creation
+        if( Vector2.SqrMagnitude(_head - _tail) > MAX_LENGTH ) {
+            _isCreating = false;
+        }
+
+        updateTransform();
+
+    }
+
+    // Abstract variables are now updated into transform of object.
+    // DO NOT HANDLE TRANFORM VALUES IN OTHER METHODS.
+    private void updateTransform() {
+
+        Vector2 position2D = 0.5f * (_head + _tail);
+        transform.localPosition = new Vector3(position2D.x, _constantY, position2D.y);
+
+        transform.localScale = new Vector3(_constantXScale, 0.5f * Vector2.SqrMagnitude(_head - _tail), _constantZScale);
+
+    }
+
+}
---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour {
+
+    public float fireRate;
+
+    public GameObject Lazer;
+    public Transform LazerShotSpawn;
+
+    private float nextFire = 0.0f;
+
+    private void Update() {
+        
+        if(Input.touches.Length > 0 && Input.touches[0].phase == TouchPhase.Began) {
+            if(Time.time > nextFire) {
+
+                nextFire = Time.time + fireRate;
+
+                var lazer = Instantiate(Lazer, LazerShotSpawn.position, LazerShotSpawn.rotation);
+                lazer.transform.parent = transform;
+
+            }
+        }
+
+    }
+
+}
